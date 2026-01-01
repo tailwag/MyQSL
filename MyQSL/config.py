e2c3ebad29e17c29a9206787d401cc7d0a43d2d7
Devin Shoemaker <devin@shoemaker.info>
2026-01-01 11:53:20 -0500

    add optional value checking

--- MyQSL/config.py
+++ MyQSL/config.py
@@ -17,13 +17,19 @@ if xmlconfig.get("MyQSLConfig") is None:
     raise RuntimeError("Missing <MyQSLConfig> block in config file")
 
 
-def get_config(path):
+def get_config(path, required=True):
     path_list = path.split("/")
+    last_value = path_list[-1]
 
     # Settings/Database
     current_level = xmlconfig["MyQSLConfig"]
     for i in path_list:
         if current_level.get(i) is None:
+            # allow for optional values
+            # can use this to check if a value is defined
+            if i == last_value and required == False:
+                return None
+
             raise RuntimeError("BAD CONFIG: <" + i + "> config block not found")
 
         current_level = current_level[i]
