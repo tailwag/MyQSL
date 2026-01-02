37282c5b066757a9d7c365c6667e355afbded9f9
Devin Shoemaker <devin@shoemaker.info>
2026-01-02 09:53:54 -0500

    directory sanity checking

--- MyQSL/thumbnail.py
+++ MyQSL/thumbnail.py
@@ -6,7 +6,24 @@ from MyQSL.config import get_config
 back_path = get_config("Settings/QSLCard/BackdropPath")
 thumb_path = get_config("Settings/QSLCard/ThumbnailPath")
 
+
+def check_dirs():
+    if not os.path.isdir(back_path):
+        os.makedirs(back_path)
+
+    if not os.path.isdir(back_path):
+        raise RuntimeError("Backdrop path does not exist, and the program was not able to create it.")
+
+    if not os.path.isdir(thumb_path):
+        os.makedirs(thumb_path)
+
+    if not os.path.isdir(thumb_path):
+        raise RuntimeError("Thumbnail path does not exist, and the program was not able to create it.")
+
+
 def thumbnail_check():
+    check_dirs()
+
     backdrops = os.listdir(back_path)
     thumbnails = os.listdir(thumb_path)
 
